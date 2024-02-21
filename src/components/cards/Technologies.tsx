c559c31ea691568c7fb20979c6aeebd1ac950142
Ali Shahbazi <ali.vortex799@gmail.com>
2024-02-20 22:32:47 -0500

    Responsive expertise card for 1024 px

--- src/components/cards/Technologies.tsx
+++ src/components/cards/Technologies.tsx
@@ -6,8 +6,8 @@ type Props = {
 
 export default function Technologies({ keyword }: Props) {
 	  return (
-			<div className="technologyIcons flex flex-wrap justify-center items-center">
-				  <div className="relative overflow-auto sm:w-3 sm:h-3 md:w-4 md:h-4 lg:w-8 lg:h-8 xl:w-10 xl:h-10">
+			<div className="flex flex-wrap justify-center items-center">
+				  <div className="technologyIcons">
 						<Image
 							  src={`/images/keywords/${keyword}.png`}
 							  alt={`${keyword}`}
