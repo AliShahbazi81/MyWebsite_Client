8603cfd19cad129346cdabac10e937d951ef92f0
Ali Shahbazi <ali.vortex799@gmail.com>
2024-02-25 22:24:35 -0500

    Fix expertise card bug which only used to functional with right navigation

--- src/components/cards/MyExpertise.tsx
+++ src/components/cards/MyExpertise.tsx
@@ -8,128 +8,139 @@ import Image from "next/image";
 import "../../app/css/expertise.scss"
 
 const cardData = [
-	  {
-			id: 'softwareDevelopment',
-			nextLine: true,
-			expertise: 'Software',
-			secondExpertise: 'Development',
-			description: (
-				  <p className={'descriptionText'}>
-						Seasoned in .NET with <span className="text-primary"><b>6+ years</b></span> crafting scalable solutions, adept in <span className="text-primary"><b>OOP</b></span>, <span className="text-primary"><b>SOLID principles</b></span>, and best coding practices.
-				  </p>
-			),
-			technologies: [],
-	  },
-	  {
-			id: 'backend',
-			nextLine: false,
-			expertise: 'Backend',
-			description: (
-				  <p className={'descriptionText'}>
-						Expert in <span className="text-primary"><b>.NET</b></span> with a strong focus on robust, scalable backends, complemented by proficiency in <span className="text-primary"><b>Django</b></span> and <span className="text-primary"><b>Spring Boot</b></span>.
-				  </p>
-			),
-			technologies: ['DotNetCore', 'Django', 'SpringBoot'],
-	  },
-	  {
-			id: 'frontend',
-			nextLine: false,
-			expertise: 'Frontend',
-			description: (
-				  <p className={'descriptionText'}>
-						Skilled in <span className="text-primary"><b>React</b></span>, <span className="text-primary"><b>Next.js</b></span>, and <span className="text-primary"><b>TypeScript</b></span> creating responsive, user-focused interfaces that seamlessly integrate with dynamic backends.
-				  </p>
-			),
-			technologies: ['React', 'Redux', 'NextJs', 'MaterialUI', 'Tailwind', 'JavaScript', 'TypeScript'],
-	  },
-	  {
-			id: 'database',
-			nextLine: false,
-			expertise: 'Database',
-			description: (
-				  <p className={'descriptionText'}>
-						Proficient with <span className="text-primary"><b>SQL Server</b></span>, <span className="text-primary"><b>PostgreSQL</b></span>,<span className="text-primary"><b> MongoDB</b></span>, optimizing data storage and retrieval for high-performance, scalable applications.
-				  </p>
-			),
-			technologies: ['MongoDb', 'PostgresSql', 'Redis', 'SqlServer', 'MySql'],
-	  },
+	{
+		id: 'softwareDevelopment',
+		nextLine: true,
+		expertise: 'Software',
+		secondExpertise: 'Development',
+		description: (
+			<p className={'descriptionText'}>
+				Seasoned in .NET with <span className="text-primary"><b>6+ years</b></span> crafting scalable solutions,
+				adept in <span className="text-primary"><b>OOP</b></span>, <span className="text-primary"><b>SOLID principles</b></span>,
+				and best coding practices.
+			</p>
+		),
+		technologies: [],
+	},
+	{
+		id: 'backend',
+		nextLine: false,
+		expertise: 'Backend',
+		description: (
+			<p className={'descriptionText'}>
+				Expert in <span className="text-primary"><b>.NET</b></span> with a strong focus on robust, scalable
+				backends, complemented by proficiency in <span className="text-primary"><b>Django</b></span> and <span
+				className="text-primary"><b>Spring Boot</b></span>.
+			</p>
+		),
+		technologies: ['DotNetCore', 'Django', 'SpringBoot'],
+	},
+	{
+		id: 'frontend',
+		nextLine: false,
+		expertise: 'Frontend',
+		description: (
+			<p className={'descriptionText'}>
+				Skilled in <span className="text-primary"><b>React</b></span>, <span
+				className="text-primary"><b>Next.js</b></span>, and <span
+				className="text-primary"><b>TypeScript</b></span> creating responsive, user-focused interfaces that
+				seamlessly integrate with dynamic backends.
+			</p>
+		),
+		technologies: ['React', 'Redux', 'NextJs', 'MaterialUI', 'Tailwind', 'JavaScript', 'TypeScript'],
+	},
+	{
+		id: 'database',
+		nextLine: false,
+		expertise: 'Database',
+		description: (
+			<p className={'descriptionText'}>
+				Proficient with <span className="text-primary"><b>SQL Server</b></span>, <span className="text-primary"><b>PostgreSQL</b></span>,<span
+				className="text-primary"><b> MongoDB</b></span>, optimizing data storage and retrieval for
+				high-performance, scalable applications.
+			</p>
+		),
+		technologies: ['MongoDb', 'PostgresSql', 'Redis', 'SqlServer', 'MySql'],
+	},
 ];
 
 export default function MyExpertise() {
-	  const [currentIndex, setCurrentIndex] = useState(0);
-	  const totalCards = cardData.length;
+	const [currentIndex, setCurrentIndex] = useState(0);
+	const [direction, setDirection] = useState('prev');
 
-	  const nextCard = () => {
-			setCurrentIndex((prevIndex) => (prevIndex + 1) % totalCards);
-	  };
+	const totalCards = cardData.length;
 
-	  const prevCard = () => {
-			setCurrentIndex((prevIndex) => {
-				  return prevIndex - 1 < 0 ? totalCards - 1 : prevIndex - 1;
-			});
-	  };
+	const nextCard = () => {
+		setCurrentIndex((prevIndex) => (prevIndex + 1) % totalCards);
+		setDirection('next');
+	};
 
-	  const determineVisibleCards = () => {
-			let cardsToShow = [];
-			for (let i = 0; i < 3; i++) {
-				  let index = (currentIndex + i) % totalCards;
-				  cardsToShow.push(cardData[index]);
-			}
-			return cardsToShow;
-	  };
+	const prevCard = () => {
+		setCurrentIndex((prevIndex) => {
+			return prevIndex - 1 < 0 ? totalCards - 1 : prevIndex - 1;
+		});
+		setDirection('prev');
+	};
 
-	  const visibleCards = determineVisibleCards();
-	  return (
-			<div className={'cardEnv'}>
-				  <TransitionGroup className="cardTransition">
-						{visibleCards.map((card) => (
-							  <CSSTransition key={card.id} timeout={1000} classNames="slide">
-									<div className={`cardExpertise ${card.id + "Card"}`}>
-										  {/* Card Header*/}
-										  <CardHeader
-												expertise={card.expertise}
-												nextLine={card.nextLine} secondExpertise={card.secondExpertise}
-										  />
-										  {/*Card Body*/}
-										  <div className={'mt-4'}>
-												<TagElement {...{[card.id]: true}} />
-												<div className={'cardBody'}>
-													  <DescriptionLine>
-															{card.description}
-													  </DescriptionLine>
-												</div>
-												<TagElement {...{[card.id]: true}} />
-												{/*Technologies */}
-												<div className={'technologyEnv'}>
-													  <h1 className={'technologyTitle'}>Technologies: </h1>
-													  <div className={'flex items-center justify-center flex-wrap'}>
-															{card.technologies.map((tech) => (
-																  <Technologies key={tech} keyword={tech}/>
-															))}
-													  </div>
-												</div>
-												<div className={'break-line shine-effect'}></div>
-										  </div>
+	const determineVisibleCards = () => {
+		let cardsToShow = [];
+		for (let i = 0; i < 3; i++) {
+			let index = (currentIndex + i) % totalCards;
+			cardsToShow.push(cardData[index]);
+		}
+		return cardsToShow;
+	};
+
+	const visibleCards = determineVisibleCards();
+	return (
+		<div className={'cardEnv'}>
+			<TransitionGroup className="cardTransition">
+				{visibleCards.map((card) => (
+					<CSSTransition
+						key={card.id}
+						timeout={1000}
+						classNames={direction === 'next' ? "slide" : "slide-prev"}
+					>
+						<div className={`cardExpertise ${card.id + "Card"}`}>
+							<CardHeader
+								expertise={card.expertise}
+								nextLine={card.nextLine} secondExpertise={card.secondExpertise}
+							/>
+							<div className={'mt-4'}>
+								<TagElement {...{[card.id]: true}} />
+								<div className={'cardBody'}>
+									<DescriptionLine>
+										{card.description}
+									</DescriptionLine>
+								</div>
+								<TagElement {...{[card.id]: true}} />
+								<div className={'technologyEnv'}>
+									<h1 className={'technologyTitle'}>Technologies: </h1>
+									<div className={'flex items-center justify-center flex-wrap'}>
+										{card.technologies.map((tech) => (
+											<Technologies key={tech} keyword={tech}/>
+										))}
 									</div>
-							  </CSSTransition>
-						))}
-				  </TransitionGroup>
-				  {/* Blur Logo*/}
-				  <div className={'flex items-center justify-center'}>
-						<Image
-							  className={'blurLogo'}
-							  src={"/images/Logo.png"}
-							  alt={"Logo"}
-							  width={800}
-							  height={900}
-
-						/>
-				  </div>
-				  <div className={'navigation'}>
-						<button onClick={prevCard} className="px-2 py-1 sm:px-4 sm:py-2 shine-effect">&#60;</button>
-						<button onClick={nextCard} className="px-2 py-1 sm:px-4 sm:py-2 shine-effect">&#62;</button>
-
-				  </div>
+								</div>
+								<div className={'break-line shine-effect'}></div>
+							</div>
+						</div>
+					</CSSTransition>
+				))}
+			</TransitionGroup>
+			<div className={'flex items-center justify-center'}>
+				<Image
+					className={'blurLogo'}
+					src={"/images/Logo.png"}
+					alt={"Logo"}
+					width={800}
+					height={900}
+				/>
 			</div>
-	  );
+			<div className={'navigation'}>
+				<button onClick={prevCard} className="px-2 py-1 sm:px-4 sm:py-2 shine-effect">&#60;</button>
+				<button onClick={nextCard} className="px-2 py-1 sm:px-4 sm:py-2 shine-effect">&#62;</button>
+			</div>
+		</div>
+	);
 }
