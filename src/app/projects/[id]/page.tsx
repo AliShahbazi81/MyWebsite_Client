a0cf74de35c57c992c10d673e66f7f53810c7e0a
Ali Shahbazi <ali.vortex799@gmail.com>
2024-02-27 09:25:38 -0500

    highlight keywords and add diagram to project detail

--- src/app/projects/[id]/page.tsx
+++ src/app/projects/[id]/page.tsx
@@ -1,3 +1,4 @@
+'use client'
 import "./../../css/projectDetail.scss"
 import "./../../css/project.scss"
 import Image from "next/image";
@@ -6,13 +7,49 @@ const data = [
 	{
 		id: "car-auction",
 		name: "Car Auction",
-		explanation: "",
+		explanation: (
+			<>
+				<h2 className={'projectDetailHeading'}>Introduction to Carsties Auctions</h2>
+				<p className={'projectDetailDescription'}>
+					{/* eslint-disable-next-line react/no-unescaped-entities */}
+					The <span className='text-primary'>Carsties Auctions</span> platform is an innovative online car auction system that transforms the dynamics of vehicle trading. With a focus on creating a seamless, efficient, and transparent buying and selling experience, the project is rooted in a <span className='text-primary'>microservices architecture</span>. This approach harnesses a variety of advanced technologies and methodologies to ensure the platform's scalability, performance, and user satisfaction.
+				</p>
+
+				<h2 className={'projectDetailHeading'}>Core Features and User Experience</h2>
+				<p className={'projectDetailDescription'}>
+					Designed from the ground up, Carsties Auctions aims to streamline the car auction process. Users have the capability to list vehicles, engage in real-time bidding, and effortlessly manage their auctions. Essential features include <span className='text-primary'>secure user authentication</span> and authorization, <span className='text-primary'>comprehensive auction management</span>, <span className='text-primary'>real-time bidding</span> mechanics, and <span className='text-primary'>advanced auction listings and filters</span>. The platform also emphasizes keeping users informed with <span className='text-primary'>instant notifications</span> on auction updates, fostering an engaged and informed community.
+				</p>
+
+				<h2 className={'projectDetailHeading'}>Architectural Overview</h2>
+				<p className={'projectDetailDescription'}>
+					{/* eslint-disable-next-line react/no-unescaped-entities */}
+					The <span className='text-primary'>architecture</span> of Carsties Auctions is a testament to modern <span className='text-primary'>software engineering</span>, comprising multiple <span className='text-primary'>microservices</span> each dedicated to a specific function - <span className='text-primary'>Auction Service</span>, <span className='text-primary'>Bidding Service</span>, <span className='text-primary'>Identity Service</span>, <span className='text-primary'>Notification Service</span>, <span className='text-primary'>Search Service</span>, and <span className='text-primary'>Gateway Service</span>. This modular design significantly enhances the application's <span className='text-primary'>maintainability</span>, <span className='text-primary'>scalability</span>, and facilitates <span className='text-primary'>agile development</span> and <span className='text-primary'>deployment</span> processes.
+				</p>
+				<Image 
+					className={'projectDetailDiagram'}
+					src={`/images/diagrams/car-auction-diagram.svg`} 
+					alt={"car-auction-diagram"} 
+					width={1300} 
+					height={1200}
+				/>
+
+				<h2 className={'projectDetailHeading'}>Technological Stack</h2>
+				<p className={'projectDetailDescription'}>
+					On the technological front, the project leverages <span className='text-primary'>React</span> and <span className='text-primary'>Next.js</span> to craft a dynamic and responsive frontend experience. The backend architecture is powered by <span className='text-primary'>.NET 7</span>, with <span className='text-primary'>Entity Framework Core</span> for ORM, <span className='text-primary'>MassTransit</span> coupled with <span className='text-primary'>RabbitMQ</span> for efficient messaging, and <span className='text-primary'>gRPC</span> for seamless inter-service communication. Data storage solutions include <span className='text-primary'>PostgreSQL</span> and <span className='text-primary'>MongoDB</span>, addressing both structured and bid management data needs. The entire infrastructure is containerized using <span className='text-primary'>Docker</span>, with <span className='text-primary'>Docker Compose</span> handling orchestration to simplify deployment and scaling operations.
+				</p>
+
+				<h2 className={'projectDetailHeading'}>Project Impact and Personal Contributions</h2>
+				<p className={'projectDetailDescription'}>
+					This project not only showcases the application of cutting-edge technologies in real-world scenarios but also highlights my comprehensive skill set in <span className='text-primary'>architecting</span>, <span className='text-primary'>developing</span>, and <span className='text-primary'>deploying</span> complex web applications. My contributions spanned across all phases of the project - from initial design and <span className='text-primary'>architecture</span> to <span className='text-primary'>development</span>, and <span className='text-primary'>deployment</span>, ensuring a robust, scalable, and user-centric platform. Through Carsties Auctions, I have demonstrated an adeptness in navigating the complexities of <span className='text-primary'>software engineering</span>, pushing the boundaries of traditional web applications, and delivering a product that stands as a benchmark for quality and innovation in the online car auction space.
+				</p>
+			</>
+		),
 		backend: ['DotNetCore'],
 		frontend: ['NextJs', 'Tailwind', 'MobX', "TypeScript"],
 		database: ['PostgresSql', 'MongoDb'],
 		messageBus: ['RabbitMq'],
 		container: ["Docker"],
-		tools: [],
+		tools: ['SignalR', 'MediatR'],
 		versionControl: ["Git"]
 	},
 	{
@@ -66,7 +103,7 @@ const data = [
 	}
 ]
 
-export default async function ProjectPage({params}: { params: { id: string } }) {
+export default function ProjectPage({params}: { params: { id: string } }) {
 	const project = data.find(project => project.id === params.id)
 
 	return (
@@ -80,11 +117,7 @@ export default async function ProjectPage({params}: { params: { id: string } }) 
 									<h2 className={'title'}>
 										{project.name}
 									</h2>
-									<p>
-										&Lorem ipsum dolor sit amet, consectetur adipisicing elit. A aliquam atque beatae
-										deleniti deserunt dicta ducimus excepturi expedita facilis necessitatibus numquam
-										officiis quas quis sequi soluta tenetur vero, voluptas voluptates!
-									</p>
+									{project.explanation}
 								</div>
 								{/* Technologies */}
 								<div className={'projectDetailTechnology'}>
