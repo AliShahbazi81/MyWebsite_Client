b345944a9ab85e46e2bd4e2c2e8b602e05a52812
Ali Shahbazi <ali.vortex799@gmail.com>
2024-03-05 00:56:51 -0500

    Translate RideShare project to german

--- src/components/projects/explanations/RideShare.tsx
+++ src/components/projects/explanations/RideShare.tsx
@@ -1,18 +1,21 @@
 import Image from "next/image";
 import React from "react";
+import {Trans, useTranslation} from "next-i18next";
 
 export default function RideShare() {
+	const {t} = useTranslation('common');
 return(
 	<>
-		<h2 className={'projectDetailHeading'}>Comprehensive Overview of Revaal</h2>
+		<h2 className={'projectDetailHeading'}>{t('projectRideShareOverview')}</h2>
 		<p className={'projectDetailDescription'}>
-			The inception of <span className='text-primary font-normal'>Revaal</span> was driven by the ambition
-			to redefine the conventional rideshare landscape. This project was meticulously crafted using <span
-			className='text-primary font-normal'>.NET Core</span> for the backend and <span
-			className='text-primary font-normal'>React</span> for the frontend, embodying the pinnacle of modern
-			web application development. It was conceived to address the gaping need for a reliable, efficient,
-			and user-centric platform that simplifies the process of sharing rides, with an emphasis on
-			security, scalability, and a seamless user experience.
+			<Trans 
+			i18nKey={'projectRideShareOverviewDescription'}
+			components={{
+				1: <span className="text-primary font-normal"><b/></span>,
+				3: <span className="text-primary font-normal"><b/></span>,
+				5: <span className="text-primary font-normal"><b/></span>,
+			}}
+			/>
 		</p>
 		<Image
 			className={'projectDetailDiagram'}
@@ -25,66 +28,60 @@ return(
 			quality={100}
 		/>
 
-		<h2 className={'projectDetailHeading'}>Innovative Feature Set</h2>
+		<h2 className={'projectDetailHeading'}>{t('projectRideShareFeatures')}</h2>
 		<p className={'projectDetailDescription'}>
-			{/* eslint-disable-next-line react/no-unescaped-entities */}
-			At the heart of Revaal lies its innovative feature set, designed from the ground up to cater to both
-			{/* eslint-disable-next-line react/no-unescaped-entities */}
-			drivers and passengers. The system's <span className='text-primary font-normal'>dynamic ride matching algorithm</span> not
-			only prioritizes geographical proximity but also factors in user preferences and ratings, ensuring a
-			match that is both convenient and safe. The <span className='text-primary font-normal'>real-time ride tracking</span> feature
-			empowers users with information at their fingertips, enhancing trust and reliability. Meanwhile,
-			the <span className='text-primary font-normal'>integrated payment system</span>, bolstered by
-			{/* eslint-disable-next-line react/no-unescaped-entities */}
-			Stripe's robust infrastructure, offers a wide array of payment options, ensuring transactions are
-			smooth and secure.
+			<Trans
+				i18nKey={'projectRideShareFeaturesDescription'}
+				components={{
+					1: <span className="text-primary font-normal"><b/></span>,
+					3: <span className="text-primary font-normal"><b/></span>,
+					5: <span className="text-primary font-normal"><b/></span>,
+				}}
+			/>
 		</p>
 
-		<h2 className={'projectDetailHeading'}>Deep Dive into Technology</h2>
+		<h2 className={'projectDetailHeading'}>{t('projectRideShareTechnology')}</h2>
 		<p className={'projectDetailDescription'}>
-			{/* eslint-disable-next-line react/no-unescaped-entities */}
-			The architectural backbone of Revaal is its <span className='text-primary font-normal'>microservices architecture</span>,
-			{/* eslint-disable-next-line react/no-unescaped-entities */}
-			enabling modular development, ease of maintenance, and scalability. The backend's reliance on <span
-			className='text-primary font-normal'>ASP.NET Core</span> is a testament to our commitment to
-			performance and security, while the frontend use of <span className='text-primary font-normal'>React and TypeScript</span> ensures
-			a dynamic and responsive user experience. Our database choice, <span
-			className='text-primary font-normal'>SQL Server</span>, alongside <span
-			className='text-primary font-normal'>MongoDB</span> for document-oriented needs, reflects our
-			dedication to robust data management and flexibility.
+			<Trans
+				i18nKey={'projectRideShareTechnologyDescription'}
+				components={{
+					1: <span className="text-primary font-normal"><b/></span>,
+					3: <span className="text-primary font-normal"><b/></span>,
+					5: <span className="text-primary font-normal"><b/></span>,
+					7: <span className="text-primary font-normal"><b/></span>,
+					9: <span className="text-primary font-normal"><b/></span>,
+				}}
+			/>
 		</p>
 
-		<h2 className={'projectDetailHeading'}>Addressing Security and Privacy</h2>
+		<h2 className={'projectDetailHeading'}>{t('projectRideShareSecurity')}</h2>
 		<p className={'projectDetailDescription'}>
-			{/* eslint-disable-next-line react/no-unescaped-entities */}
-			Security and privacy stand as the pillars of the Revaal platform. Implementing <span
-			className='text-primary font-normal'>JWT</span> for authentication and adhering to <span
-			className='text-primary font-normal'>OAuth 2.0</span> standards for third-party integrations, we've
-			established a secure environment that users can trust. Our comprehensive security measures ensure
-			that every transaction and user interaction is conducted with the highest regard for privacy and
-			data protection.
+			<Trans
+				i18nKey={'projectRideShareSecurityDescription'}
+				components={{
+					1: <span className="text-primary font-normal"><b/></span>,
+					3: <span className="text-primary font-normal"><b/></span>,
+				}}
+			/>
 		</p>
 
-		<h2 className={'projectDetailHeading'}>Overcoming Development Hurdles</h2>
+		<h2 className={'projectDetailHeading'}>{t('projectRideShareChallenges')}</h2>
 		<p className={'projectDetailDescription'}>
-			The journey of developing Revaal was marked by numerous technical and design challenges. Achieving
-			seamless communication across microservices required a sophisticated <span
-			className='text-primary font-normal'>API gateway</span> solution, while ensuring data consistency
-			across distributed services necessitated innovative approaches like <span
-			className='text-primary font-normal'>event sourcing</span> and <span
-			className='text-primary font-normal'>CQRS patterns</span>. Tackling the dynamic nature of the
-			frontend presented its own set of challenges, met through advanced state management techniques and a
-			component-driven architecture.
+			<Trans
+				i18nKey={'projectRideShareChallengesDescription'}
+				components={{
+					1: <span className="text-primary font-normal"><b/></span>,
+					3: <span className="text-primary font-normal"><b/></span>,
+					5: <span className="text-primary font-normal"><b/></span>,
+				}}
+			/>
 		</p>
 
-		<h2 className={'projectDetailHeading'}>Reflections on Personal Contributions</h2>
+		<h2 className={'projectDetailHeading'}>{t('projectRideShareReflections')}</h2>
 		<p className={'projectDetailDescription'}>
-			My involvement in Revaal spanned the entirety of its lifecycle, from conceptual design to
-			deployment. Leading the architectural design, I navigated the complexities of integrating multiple
-			services into a cohesive system. My work on the frontend introduced innovative features that
-			enhanced user interactions, while backend development focused on creating scalable, secure services.
-			This project was a profound learning journey, deepening my expertise in full-stack development and
-			reinforcing the importance of user-centric design in creating impactful digital solutions.
+			<Trans
+				i18nKey={'projectRideShareReflectionsDescription'}
+			/>
 		</p>
 	</>
 )
